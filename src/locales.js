f99a581e9682347a935ecf7db02258ccf141104c
Pabloledesma <=>
2017-01-04 09:02:19 -0500

    se agregaron las traducciónes del footer pero se cometió un error aun imperceptible

--- src/locales.js
+++ src/locales.js
@@ -16,6 +16,121 @@ export default {
 
 			latestNewsPage: {
 				title: 'Latest News'
+			},
+
+			footer: {
+				col1: {
+					title: 'Customer Service',
+					faq: {
+						label: 'FAQ',
+						url: 'https://www.copaair.com/en/web/gs/faq'
+					},
+					reservationCenter: {
+						label: 'Sales ofices and reservation center',
+						url: 'https://www.copaair.com/en/web/gs/reservation-center'
+					},
+					refundRequest: {
+						label: 'Request and refund',
+						url: 'https://www.copaair.com/en/web/gs/refund-request'
+					},
+					baggageClaim: {
+						label: 'Baggage Claim',
+						url: 'https://www.copaair.com/en/web/gs/baggage-claim'
+					},
+					forgottenItems: {
+						label: 'Report your forgotten items',
+						url: 'https://www.copaair.com/en/web/gs/forgotten-items'
+					},
+					flightNotifications: {
+						label: 'Flight notifications',
+						url: 'https://www.copaair.com/en/web/gs/flight-notifications'
+					},
+					optionsForCheckin: {
+						label: 'Options for checking',
+						url: 'https://www.copaair.com/en/web/gs/checkin-options'
+					},
+					commentsAndComplaints: {
+						label: 'Comments / complaints',
+						url: 'https://www.copaair.com/en/web/gs/comments-and-complaints'
+					},
+					contactUs: {
+						label: 'Contact us',
+						url: 'https://www.copaair.com/en/web/gs/contact-us'
+					}
+
+				},
+				col2: {
+					title: 'About Copa Airlines',
+					ourHistory: {
+						label: 'Our history',
+						url: 'https://www.copaair.com/en/web/gs/our-history'
+					},
+					awards: {
+						label: 'Awards',
+						url: 'https://www.copaair.com/en/web/gs/awards'
+					},
+					corporateSocialResponsability: {
+						label: 'Corporate social responsability',
+						url: 'https://www.copaair.com/en/web/gs/corporate-social-responsibility'
+					},
+					alliances: {
+						label: 'Alliances',
+						url: 'https://www.copaair.com/en/web/gs/alliances'
+					},
+					aeroRepublica: {
+						label: 'Aero Republica',
+						url: 'https://www.copaair.com/en/web/gs/aero-republica'
+					},
+					investorRelations: {
+						label: 'Investor Relations',
+						url: 'http://investor.shareholder.com/copa/'
+					},
+					hubOfTheAmericas: {
+						label: 'Hub of the Americas',
+						url: 'https://www.copaair.com/en/web/gs/hub-of-the-americas'
+					},
+					fleet: {
+						label: 'Fleet',
+						url: 'https://www.copaair.com/en/web/gs/fleet'
+					},
+					workWithUs: {
+						label: 'Work with us',
+						url: 'https://www.copaair.com/en/web/gs/work-with-us'
+					},
+					securePurchases: {
+						label: 'Secure purchases',
+						url: 'https://www.copaair.com/en/web/gs/safe-purchases'
+					},
+					news: {
+						label: 'News',
+						url: 'https://www.copaair.com/en/web/gs/news'
+
+					}
+				},
+				col3: {
+					title: 'Legal Information',
+					customerCommitment: {
+						label: 'Customer commitment',
+						url: 'https://www.copaair.com/en/web/gs/customer-commitment'
+					},
+					affidavitTravelToCuba: {
+						label: 'Affidavit for travel to Cuba',
+						url: 'https://www.copaair.com/en/web/gs/affidavit-travel-to-cuba'
+					},
+					lengthyTarmacsDelays: {
+						label: 'Lengthy tarmacs delays',
+						url: 'https://www.copaair.com/en/web/gs/lengthy-tarmac-delays'
+					},
+					eDBVBrazil: {
+						label: 'Electronic declaration (e-DBV-Brazil)',
+						url: 'https://www.copaair.com/en/web/gs/electronic-declaration-passenger-goods-brazil'
+					},
+					digitalTaxReceiptMexico: {
+						label: 'Digital receipt for tax purposes (Mexico)',
+						url: 'https://www.copaair.com/en/web/gs/digital-tax-receipt-mexico'
+					}
+				}
+				
 			}
 		},
 		es: {
@@ -34,6 +149,121 @@ export default {
 
 			latestNewsPage: {
 				title: 'Últimas Noticias'
+			},
+
+			footer: {
+				col1: {
+					title: 'Atención al cliente',
+					faq: {
+						label: 'Preguntas frecuentes',
+						url: 'https://www.copaair.com/es/web/gs/preguntas-frecuentes'
+					},
+					reservationCenter: {
+						label: 'Oficinas de ventas y centro de reservaciones',
+						url: 'https://www.copaair.com/es/web/gs/centro-de-reservaciones'
+					},
+					refundRequest: {
+						label: 'Solicite un reembolso',
+						url: 'https://www.copaair.com/es/web/gs/solicite-un-reembolso'
+					},
+					baggageClaim: {
+						label: 'Reclamo de equipaje',
+						url: 'https://www.copaair.com/es/web/gs/reclamo-de-equipaje'
+					},
+					forgottenItems: {
+						label: 'Reporte su artículo olvidado',
+						url: 'https://www.copaair.com/es/web/gs/articulos-olvidados'
+					},
+					flightNotifications: {
+						label: 'Notificaciones de vuelo',
+						url: 'https://www.copaair.com/es/web/gs/notificaciones-de-vuelo'
+					},
+					optionsForCheckin: {
+						label: 'Opciones de registro',
+						url: 'https://www.copaair.com/es/web/gs/opciones-checkin'
+					},
+					commentsAndComplaints: {
+						label: 'Sugerencias y reclamos',
+						url: 'https://www.copaair.com/es/web/gs/sugerencias-y-reclamos'
+					},
+					contactUs: {
+						label: 'Contactenos',
+						url: 'https://www.copaair.com/es/web/gs/contactenos'
+					}
+
+				},
+				col2: {
+					title: 'ACERCA DE COPA AIRLINES',
+					ourHistory: {
+						label: 'Nuestra historia',
+						url: 'https://www.copaair.com/es/web/gs/nuestra-historia'
+					},
+					awards: {
+						label: 'Reconocimientos',
+						url: 'https://www.copaair.com/es/web/gs/reconocimientos'
+					},
+					corporateSocialResponsability: {
+						label: 'Responsabilidad social empresarial',
+						url: 'https://www.copaair.com/es/web/gs/responsabilidad-social-empresarial'
+					},
+					alliances: {
+						label: 'Alianzas',
+						url: 'https://www.copaair.com/es/web/gs/alianzas'
+					},
+					aeroRepublica: {
+						label: 'Aero República',
+						url: 'https://www.copaair.com/es/web/gs/aero-republica'
+					},
+					investorRelations: {
+						label: 'Relaciones con los inversionistas',
+						url: 'http://investor.shareholder.com/copa/'
+					},
+					hubOfTheAmericas: {
+						label: 'Hub de las Américas',
+						url: 'https://www.copaair.com/es/web/gs/hub-de-las-americas'
+					},
+					fleet: {
+						label: 'Flota',
+						url: 'https://www.copaair.com/es/web/gs/flota'
+					},
+					workWithUs: {
+						label: 'Trabaje con nosotros',
+						url: 'https://www.copaair.com/es/web/gs/trabaje-con-nosotros'
+					},
+					securePurchases: {
+						label: 'Compras seguras',
+						url: 'https://www.copaair.com/es/web/gs/compras-seguras'
+					},
+					news: {
+						label: 'Noticias',
+						url: 'https://www.copaair.com/es/web/gs/noticias'
+
+					}
+				},
+				col3: {
+					title: 'INFORMACIÓN LEGAL',
+					customerCommitment: {
+						label: 'Compromiso con el pasajero',
+						url: 'https://www.copaair.com/es/web/gs/compromiso-pasajero'
+					},
+					affidavitTravelToCuba: {
+						label: 'Declaración jurada de viajes a Cuba',
+						url: 'https://www.copaair.com/es/web/gs/declaracion-jurada-cuba'
+					},
+					lengthyTarmacsDelays: {
+						label: 'Plan de demoras prolongadas',
+						url: 'https://www.copaair.com/es/web/gs/demoras-prolongadas'
+					},
+					eDBVBrazil: {
+						label: 'Declaración electrónica (e-DBV-Brasil)',
+						url: 'https://www.copaair.com/es/web/gs/declaracion-electronica-de-bienes-brasil'
+					},
+					digitalTaxReceiptMexico: {
+						label: 'Comprobante fiscal digital de México',
+						url: 'https://www.copaair.com/es/web/gs/comprobante-fiscal-digital-mexico'
+					}
+				}
+				
 			}
 		},
 		pt: {
@@ -52,6 +282,120 @@ export default {
 
 			latestNewsPage: {
 				title: 'Últimas Notícias'
+			},
+			footer: {
+				col1: {
+					title: 'ATENDIMENTO AO CLIENTE',
+					faq: {
+						label: 'Preguntas frecuentes',
+						url: 'https://www.copaair.com/pt/web/gs/preguntas-frecuentes'
+					},
+					reservationCenter: {
+						label: 'Escritórios de vendas e central de reservas',
+						url: 'https://www.copaair.com/pt/web/gs/central-de-reservas'
+					},
+					refundRequest: {
+						label: 'Solicitar um reembolso',
+						url: 'https://www.copaair.com/pt/web/gs/solicitacao-de-reembolso'
+					},
+					baggageClaim: {
+						label: 'Retirada de bagagem',
+						url: 'https://www.copaair.com/pt/web/gs/Retirada-de-bagagem'
+					},
+					forgottenItems: {
+						label: 'Informe seu item esquecido',
+						url: 'https://www.copaair.com/pt/web/gs/itens-esquecidos'
+					},
+					flightNotifications: {
+						label: 'Notificações de voos',
+						url: 'https://www.copaair.com/pt/web/gs/notificacoes-de-voo'
+					},
+					optionsForCheckin: {
+						label: 'Opções de registro',
+						url: 'https://www.copaair.com/pt/web/gs/opcoes-checking'
+					},
+					commentsAndComplaints: {
+						label: 'Sugestões e reclamações',
+						url: 'https://www.copaair.com/pt/web/gs/sugestoes-e-reclamacoes'
+					},
+					contactUs: {
+						label: 'Fale conosco',
+						url: 'https://www.copaair.com/pt/web/gs/fale-conosco'
+					}
+
+				},
+				col2: {
+					title: 'SOBRE A COPA AIRLINES',
+					ourHistory: {
+						label: 'Nossa historia',
+						url: 'https://www.copaair.com/pt/web/gs/nossa-historia'
+					},
+					awards: {
+						label: 'Reconhecimentos',
+						url: 'https://www.copaair.com/pt/web/gs/reconhecimentos'
+					},
+					corporateSocialResponsability: {
+						label: 'Responsabilidade empresarial',
+						url: 'https://www.copaair.com/pt/web/gs/responsabilidade-social-corporativa'
+					},
+					alliances: {
+						label: 'Alianças',
+						url: 'https://www.copaair.com/pt/web/gs/aliancas'
+					},
+					aeroRepublica: {
+						label: 'Aero República',
+						url: 'https://www.copaair.com/pt/web/gs/aero-republica'
+					},
+					investorRelations: {
+						label: 'Relações com investidores',
+						url: 'http://investor.shareholder.com/copa/'
+					},
+					hubOfTheAmericas: {
+						label: 'Centro de conexões das Américas',
+						url: 'https://www.copaair.com/pt/web/gs/centro-de-conexoes'
+					},
+					fleet: {
+						label: 'Frota',
+						url: 'https://www.copaair.com/es/web/gs/frota'
+					},
+					workWithUs: {
+						label: 'Trabalhe conosco',
+						url: 'https://www.copaair.com/pt/web/gs/trabalhe-conosco'
+					},
+					securePurchases: {
+						label: 'Compras seguras',
+						url: 'https://www.copaair.com/pt/web/gs/compras-seguras'
+					},
+					news: {
+						label: 'Noticias',
+						url: 'https://www.copaair.com/pt/web/gs/noticias'
+
+					}
+				},
+				col3: {
+					title: 'INFORMAÇÃO LEGAL',
+					customerCommitment: {
+						label: 'Compromisso com o cliente',
+						url: 'https://www.copaair.com/pt/web/gs/compromisso-cliente'
+					},
+					affidavitTravelToCuba: {
+						label: 'Declaración jurada de viajes a Cuba',
+						url: 'https://www.copaair.com/pt/web/gs/depoimento-viagens-a-cuba'
+					},
+					lengthyTarmacsDelays: {
+						label: 'Plano para atrasos prolongados na pista',
+						url: 'https://www.copaair.com/pt/web/gs/atrasos-prolongados'
+					},
+					eDBVBrazil: {
+						label: 'Declaração eletrônica (e-DBV-Brasil)',
+						url: 'https://www.copaair.com/pt/web/gs/declaracao-de-passageiros-de-bens-electronicos-brasil'
+					},
+					digitalTaxReceiptMexico: {
+						label: 'Comprovante fiscal digital de México',
+						url: 'https://www.copaair.com/pt/web/gs/recibo-digitais-mexico'
+					}
+				}
+				
 			}
  		}
-}
+};
