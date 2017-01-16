4051794ed553697b415f69df593e7a22e4b90c42
Pablo Ledesma <pabloledes83@gmail.com>
2017-01-16 18:01:16 -0500

    menú superior en proceso...

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -1,28 +1,23 @@
 <template>
-	<nav>
-        <div id="toggle-menu">
-            <button class="navbar-toggler hidden-sm-up collapsed" type="button" data-toggle="collapse" data-target="#navbar-header" aria-controls="navbar-header" aria-expanded="false">
-                ☰
-            </button>
-        </div>
-        <ul class="top-menu">
+	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
             <li>
-                <router-link :to="$t('topMenu.latestNews.url')">
+                <router-link :to="$t('topMenu.latestNews.url')" class="page-scroll">
                   {{ $t("topMenu.latestNews.text") }}
                 </router-link>
             </li>
             <li>
-                <router-link :to="$t('topMenu.pressReleases.url')">
+                <router-link :to="$t('topMenu.pressReleases.url')" class="page-scroll">
                   {{ $t("topMenu.pressReleases.text") }}
                 </router-link>
             </li>
             <li>
-                <router-link :to="$t('topMenu.assistance.url')">
+                <router-link :to="$t('topMenu.assistance.url')" class="page-scroll">
                   {{ $t("topMenu.assistance.text") }}
                 </router-link>
             </li>
             <li>
-                <router-link :to="$t('topMenu.about.url')">
+                <router-link :to="$t('topMenu.about.url')" class="page-scroll">
                   {{ $t('topMenu.about.text') }}
                 </router-link>
             </li>
@@ -31,14 +26,34 @@
                   {{ $t("topMenu.copaCom.text") }}
                 </a>
             </li>
+          <!--   <li>
+                <div class="form-group">
+                    <div class="input-group">
+                        <div class="input-group-addon"><i class="fa fa-globe fa-3"></i></div>
+                        <select class="form-control" v-model="currentLang" @change="changeLanguage(currentLang)" v-cloak>
+                            <option value="en">{{ $t("optionLabel.english") }}</option>
+                            <option value="es">{{ $t("optionLabel.spanish") }}</option>
+                            <option value="pt">{{ $t("optionLabel.portuguese") }}</option>
+                        </select>
+                    </div>
+                </div>
+            </li> -->
         </ul>
-	</nav>
+	</div>
 </template>
 
 <script>
 export default {
   
-    name: 'TopMenu'
+    name: 'TopMenu',
+    data(){
+        return { currentLang: 'es' }
+    },
+    methods: {
+        changeLanguage(){
+            console.log('lang changed!!')
+        }
+    }
 
 }
 </script>
