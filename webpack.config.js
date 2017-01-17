e165f71493928a76c540a922823873ad31dc89ae
Pabloledesma <=>
2017-01-17 11:08:58 -0500

    para evitar la caché del servidor, se codificó el build.js | se modificó la ruta raiz en el index para los recursos

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: '[hash].build.js'
   },
   module: {
     rules: [
